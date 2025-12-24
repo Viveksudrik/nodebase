ac4e8aa26891ed1481bf7b883c4f99f50e2f4106
Viveksudrik <gautamsudrik13@gmail.com>
2025-12-24 19:11:56 +0530

    10-payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,15 @@
+import { checkout, polar, portal } from '@polar-sh/better-auth'
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { db } from "@/lib/db";
+import { polarClient } from "@/lib/polar";
 
 // Trim any accidental quotes in the base URL to avoid invalid URL errors
 const baseURL =
 	(process.env.BETTER_AUTH_URL ??
 		process.env.NEXT_PUBLIC_APP_URL ??
 		process.env.AUTH_URL ??
-		"http://localhost:3000").replace(/^['"]|['"]$/g, "");
+		"http://localhost:3000").replace(/^["']|["']$/g, "");
 
 export const auth = betterAuth({
 	baseURL,
@@ -18,6 +20,23 @@ export const auth = betterAuth({
 		enabled: true,
 		autoSignIn: true,
 	},
+	plugins: [
+		polar({
+			client: polarClient,
+			createCustomerOnSignUp: true,
+			use: [
+				checkout({
+					products: [
+						{
+							productId: "d050fbf2-b5c4-46a2-a6ff-b9bf7390b33a",
+							slug: "Nodebase-Pro",
+						}
+					],
+					successUrl: process.env.POLAR_SUCCESS_URL,
+					authenticatedUsersOnly: true,
+				}),
+				portal(),
+			],
+		})
+	],
 });
-
-
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -3,6 +3,8 @@ import { cache } from 'react';
 import { auth } from '@/lib/auth';
 import { TRPCError } from '@trpc/server';
 import { headers } from 'next/headers';
+import { polarClient } from '@/lib/polar';
+import { polar } from '@polar-sh/better-auth';
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
@@ -23,15 +25,34 @@ const t = initTRPC.create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
-export const protectedProcedure = baseProcedure.use(async({ctx, next}) =>{
-    const session = await auth.api.getSession({
-        headers: await headers(),
+export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "Unauthorized",
+    })
+  }
+  return next({ ctx: { ...ctx, auth: session } });
+});
+
+export const premiumProcedure = protectedProcedure.use(
+  async ({ ctx, next }) => {
+    const customer = await polarClient.customers.getStateExternal({
+      externalId: ctx.auth.user.id,
     });
-    if (!session) {
-        throw new  TRPCError({
-            code: "UNAUTHORIZED",
-            message: "Unauthorized",
-        }) 
+
+    if (
+      !customer.activeSubscriptions ||
+      customer.activeSubscriptions.length === 0
+    ) {
+      throw new TRPCError({
+        code: "FORBIDDEN",
+        message: "Active subscription required",
+      });
     }
-    return next({ctx: {...ctx, auth: session}});
-})
+    return next({ ctx: { ...ctx, customer } });
+  },
+);
