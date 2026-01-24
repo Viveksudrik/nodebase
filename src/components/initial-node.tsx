e2d300bf1d08791a684feb50321bee57e7e8436b
Viveksudrik <gautamsudrik13@gmail.com>
2026-01-25 01:05:32 +0530

    15: editor

---
+++ src/components/initial-node.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { NodeProps } from "@xyflow/react";
+import { PlusIcon } from "lucide-react";
+import { memo,useState } from "react";
+import { PlaceholderNode } from "./react-flow/placeholder-node";
+import { WorkflowNode } from "./workflow-node";
+
+export const InitialNode = memo((props: NodeProps) =>{
+    return (
+        <WorkflowNode showToolbar={false}>
+        <PlaceholderNode
+            {...props} 
+        >
+            <div className="cursor-pointer flex items-center justify-center">
+                <PlusIcon className="size-4"/>
+            </div>
+        </PlaceholderNode>
+        </WorkflowNode>
+    );
+});
+    
--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,8 +1,26 @@
 "use client"
-
+import { useState, useCallback } from 'react';
 import { LoadingView, ErrorView } from "@/components/entity-components";
 import { useWorkflow } from "@/features/workflows/hooks/use-workflows";
-
+import {
+    ReactFlow,
+    applyNodeChanges,
+    applyEdgeChanges,
+    addEdge,
+    type Node,
+    type Edge,
+    type NodeChange,
+    type EdgeChange,
+    type Connection,
+    Background,
+    Controls,
+    MiniMap,
+    Panel,
+    
+    } from '@xyflow/react';
+import { nodeComponents } from '@/config/node-components';
+import { AddNodeButton } from './add-node-button';
+import '@xyflow/react/dist/style.css';
 export const EditorLoading = () => {
     return <LoadingView message="Loading editor..." />
 };
@@ -11,11 +29,47 @@ export const EditorError = () => {
     return <ErrorView message="Failed to load editor" />;
 };
 
+
 export const Editor = ({ workflowId }: { workflowId: string }) => {
     const { data: workflow } = useWorkflow(workflowId);
+
+    const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+    const [edges, setEdges] = useState<Edge[]>(workflow.edges);
+
+     const onNodesChange = useCallback(
+    (changes: NodeChange[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+
     return (
-        <p>
-            {JSON.stringify(workflow, null, 2)}
-        </p>
+        <div className='size-full'>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeComponents}
+        fitView
+        // proOptions={{
+        //     hideAttribution: true,
+        // }}
+      >
+      <Background />
+      <Controls />
+      <MiniMap />
+      <Panel position="top-right">
+        <AddNodeButton />
+      </Panel>
+      </ReactFlow>
+    </div>
     );
 };
